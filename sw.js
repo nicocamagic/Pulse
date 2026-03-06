b9af77f3f5f7a4d1eb274d3a8d15450d22caa433
NICOLAS CAMARGO LIZARAZO <98283511+nicocamagic@users.noreply.github.com>
2026-03-06 17:01:30 -0500

    cambios

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
 const CACHE = 'calculadora-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
   self.clients.claim();
 });
 
